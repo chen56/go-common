84ae69ba89a1ae839a2c61f17fc82b98353fe27e
chen56 <chen56@msn.com>
2018-01-27 17:20:09 +0800

    gob

---
+++ encoding/gobx/gobx.go
@@ -0,0 +1,22 @@
+package lib
+
+import (
+	"encoding/gob"
+	"bytes"
+)
+
+func Encode(x interface{}) ([]byte, error) {
+	var buf bytes.Buffer
+	encoder := gob.NewEncoder(&buf)
+	err := encoder.Encode(x)
+	if err != nil {
+		return nil, err
+	}
+	return buf.Bytes(), nil
+}
+
+func Decode(b []byte, result interface{}) error {
+	reader := bytes.NewBuffer(b)
+	decoder := gob.NewDecoder(reader)
+	return decoder.Decode(result)
+}
---
+++ encoding/gobx/gobx_test.go
@@ -0,0 +1,26 @@
+package lib
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+type User struct {
+	Name string
+	Age  int
+}
+
+func TestEncode(t *testing.T) {
+	a := assert.New(t)
+	user := User{Name: "chen", Age: 2}
+	data, err := Encode(user)
+	a.NoError(err)
+
+	var load User
+	err = Decode(data, &load)
+	a.NoError(err)
+	fmt.Println(load)
+	a.Equal(user, load)
+}
